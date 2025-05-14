e9ccaffacb4633c2e55c43da97ae007855799ba2
joberdan88 <jonasoberdan@gmail.com>
2025-05-14 05:28:06 -0300

    Solving exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
-class Program
+class Program       
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number == 0)
+            {
+                break;
+            }
+
+            numbers.Add(number);
+        }
+        if (numbers.Count > 0)
+        {
+            int sum = numbers.Sum();
+            double average = numbers.Average();
+            int maxNumber = numbers.Max();
+
+            Console.WriteLine($"The sum is {sum}");
+            Console.WriteLine($"The average is {average}");
+            Console.WriteLine($"The largest number is {maxNumber}");
+        }
+        else
+        {
+            Console.WriteLine("No numbers were entered.");
+        }
     }
 }
+
+
