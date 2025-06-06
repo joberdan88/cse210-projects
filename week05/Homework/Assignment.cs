bfc024f98ed21ae8a5b19590ae0af533d29fa298
joberdan88 <jonasoberdan@gmail.com>
2025-06-06 00:45:38 -0300

    Finishing heritance activity

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+    //Constructor
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    //Method
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+}
